e65bb665469b1dc479cf7aa2ebfe7c19ac9f1971
Madeline Skeel <maddeskeel@gmail.com>
2024-11-16 23:05:25 -0500

    update server

--- backend/server.py
+++ backend/server.py
@@ -20,6 +20,6 @@ def upload_image():
     return jsonify({'message': f"Processed {file.filename}"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 5000)) 
+    app.run(host='0.0.0.0', port=port)
 
