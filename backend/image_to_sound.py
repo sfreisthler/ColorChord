60a147cf4e6f102c4fcb953f934c283a7402ddaf
sfreisthler <sfreisthler@gmail.com>
2024-12-17 11:35:02 -0500

    functionality to precompute the audio ffts

--- backend/image_to_sound.py
+++ backend/image_to_sound.py
@@ -1,6 +1,29 @@
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import os
+import json
+import librosa
+
+def precompute_audio_ffts(sample_folder, output_file):
+	fft_library = {}
+	for filename in os.listdir(sample_folder):
+		if filename.endswith('.wav') or filename.endswith('.mp3'):
+			file_path = os.path.join(sample_folder, filename)
+			y, sr = librosa.load(file_path, sr=8000, mono=True)
+			fft_result = np.abs(np.fft.fft(y))
+			fft_library[filename] = fft_result.tolist()
+			print(f"{filename} processed")
+
+	with open(output_file, 'w') as json_file:
+		json.dump(fft_library, json_file)
+
+	print(f"FFT data saved to {output_file}")
+
+def load_sample_library(json_file):
+	with open(json_file, 'r') as f:
+		fft_library = json.load(f)
+	return {k: np.array(v) for k, v in fft_library.items()}
 
 def slice_image(image_path, num_slices = 10):
 	image = Image.open(image_path).convert('L')
